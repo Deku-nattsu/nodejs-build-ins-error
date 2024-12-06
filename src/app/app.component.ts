fbe650175de96572c657ee70654e6f1d07b14aa4
aymende7 <aymenbousselham@gmail.com>
2024-12-06 19:30:32 +0100

    add pdfjs-dist 4.6.82
    * 4.7 builds fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as pdfjs from 'pdfjs-dist';
+pdfjs.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/4.6.82/pdf.worker.js';
 
 @Component({
   selector: 'app-root',
